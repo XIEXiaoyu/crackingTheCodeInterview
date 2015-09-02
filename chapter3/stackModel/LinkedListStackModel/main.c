dd553fd119be9a3e1b3fb82fa67a80ee605124cb
jxie007 <jxie007@e.ntu.edu.sg>
2015-09-02 20:37:47 +0800

    minor modification

--- chapter3/stackModel/LinkedListStackModel/main.c
+++ chapter3/stackModel/LinkedListStackModel/main.c
@@ -6,23 +6,7 @@
 
 int main(int argc, char** argv)
 {
-	Node node = LStackCreate(1);
-	printf("%d %p %p \n", node->value, node, node->next);
-
-	node = push(node, 2);
-	printf("%d %p %p\n", node->value, node, node->next);
-
-	node = push(node, 3);
-	printf("%d %p %p\n", node->value, node, node->next);
-
-
-	int* data = (int*)malloc(sizeof(int));	
-	Node poped = pop(node, data);
-	printf("%d\n", *data);
-	poped = pop(poped, data);
-	printf("%d\n", *data);
-
-	destroyLStack(poped);
+	
 
 	return 0;
 }
--- chapter3/stackModel/LinkedListStackModel/stack.h
+++ chapter3/stackModel/LinkedListStackModel/stack.h
@@ -9,14 +9,17 @@ struct LSNode
 
 typedef struct LSNode* Node;
 
-//制造堆栈，并且产生堆栈的第一个元素
-Node LStackCreate(int value);
+/*
+functions: 1 to create a node of the linked list stack
+		   2 the createed node can be head of the stack
+*/
+Node LSNodeCreate(int value);
 
 //压栈
-Node push(Node LSHead, int value);
+Node push(Node tail, int value);
 
 //出栈
-Node pop(Node LSHead, int* data);
+Node pop(Node tail, int* data);
 
 //毁掉整个栈
-void destroyLStack(Node LSHead);
+void destroyLStack(Node tail);
