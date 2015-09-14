e737ca338f1dd9f23ec2d02c38082d0422de2d28
jxie007 <jxie007@e.ntu.edu.sg>
2015-09-14 11:15:47 +0800

    change name 4_4 to 4_3

---
+++ chapter4/4_3/4_3_1/4_3_1.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "bst.h" 
+
+#ifndef _4_3_1_H
+#define _4_3_1_H
+
+#define LEGNTH(s) (sizeof(s) / sizeof(int))
+
+BstNode BuildBinaryTree(int array[], int len);
+
+void InorderTraversal(BstNode root);
+
+#endif
---
+++ chapter4/4_3/4_3_1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "bst.h"
+#include "4_3_1.h"
+
+int main(int argc, char* argv[])
+{
+	int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	int len = LEGNTH(array);
+
+	BstNode root = BuildBinaryTree(array, len);
+
+	InorderTraversal(root);
+	printf("\n");
+
+	return 0;
+}
